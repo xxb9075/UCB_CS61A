09a4a6dc4db551d127db0e714764d9b45aefe86e
Yuchen Xu <xxb9075@gmail.com>
2019-11-07 17:42:46 -0800

    Directory Changed 11/07/2019

--- Week 1/lab00.py
+++
@@ -1,9 +0,0 @@
-def twenty_eighteen():
-    """Come up with the most creative expression that evaluates to 2018,
-    using only numbers and the +, *, and - operators.
-
-    >>> twenty_eighteen()
-    2018
-    """
-    return 2018*(20-18)-2018
-
