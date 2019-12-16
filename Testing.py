a20a6d9c6d8a8bd9ce47573c7d922efe7e7193cb
Yuchen Xu <xxb9075@gmail.com>
2019-12-15 23:07:08 -0800

    12/15/2019

--- Testing.py
+++ Testing.py
@@ -1,41 +1,16 @@
-def cycle(f1, f2, f3):
-    """Returns a function that is itself a higher-order function.
+def trace(fn):
+    def wrapped(x):
+        print('->', fn, '(', x, ')')
+        return fn(x)
+    return wrapped
 
-    >>> def add1(x):
-    ...     return x + 1
-    >>> def times2(x):
-    ...     return x * 2
-    >>> def add3(x):
-    ...     return x + 3
-    >>> my_cycle = cycle(add1, times2, add3)
-    >>> identity = my_cycle(0)
-    >>> identity(5)
-    5
-    >>> add_one_then_double = my_cycle(2)
-    >>> add_one_then_double(1)
-    4
-    >>> do_all_functions = my_cycle(3)
-    >>> do_all_functions(2)
-    9
-    >>> do_more_than_a_cycle = my_cycle(4)
-    >>> do_more_than_a_cycle(2)
-    10
-    >>> do_two_cycles = my_cycle(6)
-    >>> do_two_cycles(1)
-    19
-    """
-    def order(x):
-        def inner(y):
-            res = y
-            num = x
-            if num != 0:
-                while num >= 3:
-                    res = f3(f2(f1(res)))
-                    num -= 3
-                if num == 2:
-                    res = f2(f1(res))
-                elif num == 1:
-                    res = f1(res)
-            return res
-        return inner
-    return order
+@trace
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-2)+fib(n-1)
+
+fib(5)
