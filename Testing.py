da718fbe55bf9ad5df7b643bdfd61aaf4f8612d4
Yuchen Xu <xxb9075@gmail.com>
2020-01-20 23:30:40 -0800

    Lab 06 01/20/2020

--- Testing.py
+++ Testing.py
@@ -1,20 +1,51 @@
-def build_successors_table(tokens):
-    """Return a dictionary: keys are words; values are lists of successors."""
+class Account:
+    """A bank account that has a non-negative balance."""
+    interest = 0.02
+    def __init__(self, account_holder):
+        self.balance = 0
+        self.holder = account_holder
+    def deposit(self, amount):
+        """Increase the account balance by amount and return the new balance."""
+        self.balance = self.balance + amount
+        return self.balance
+    def withdraw(self, amount):
+        """Decrease the account balance by amount and return the new balance."""
+        if amount > self.balance:
+            return 'Insufficient funds'
+        self.balance = self.balance - amount
+        return self.balance
 
-    table = {}
-    prev = '.'
-    for word in tokens:
-        if prev not in table:
 
-            table[prev] = [word]
-        else:
-            table[prev].append(word)
-        prev = word
-    return table
+class CheckingAccount(Account):
+    """A bank account that charges for withdrawals."""
+    withdraw_charge = 1
+    interest = 0.01
+    def withdraw(self, amount):
+        return Account.withdraw(self, amount + self.withdraw_charge)
 
-text = ['We', 'came', 'to', 'investigate', ',', 'catch', 'bad', 'guys', 'and', 'to', 'eat', 'pie', '.']
-table = build_successors_table(text)
-sorted(table)
-table['to']
-table['pie']
-table['.']
+
+class Bank:
+    """A bank has accounts
+
+    >>> bank = Bank()
+    >>> john = bank.open_account("John", 10)
+    >>> jack = bank.open_account("Jack", 5, CheckingAccount)
+    >>> john. interest
+    >>> 0.02
+    >>> jack. interest
+    >>> 0.01
+    >>> bank.pay_interest()
+    >>> john.pay_interest()
+    >>> john.balance
+    >>> 10.2
+    """
+    def __init__(self):
+        self.accounts = []
+
+    def open_account(self, holder, amount, kind = Account):
+        account = kind(holder)
+        account.deposit(amount)
+        self.accounts.append(account)
+        return account
+
+a = Bank()
